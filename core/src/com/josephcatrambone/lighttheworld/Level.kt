011e53147ce13b4135421f65b4a57c75160ea8e8
Joseph Catrambone <jo.jcat@gmail.com>
2017-11-04 15:01:37 -0700

    Change how touches are handled and simplify the implementation.

--- core/src/com/josephcatrambone/lighttheworld/Level.kt
+++ core/src/com/josephcatrambone/lighttheworld/Level.kt
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
@@ -22,6 +23,13 @@ class Level(mapDescription: String) : Table() {
 	val buttons = mutableListOf<LightTile>()
 	val tableWidth:Int
 	val tableHeight:Int
+	val delayToggle = 0.05f;
+	var delayToNextToggle: Float = 0.05f
+
+	private val pendingToggles = mutableListOf<LightTile>()
+	override fun isTouchable(): Boolean {
+		return super.isTouchable() and pendingToggles.isEmpty() // We don't want to be messed with while we're changing.
+	}
 
 	init {
 		this.debug = true;
@@ -42,6 +50,7 @@ class Level(mapDescription: String) : Table() {
 		tableWidth = tokens[0].toInt()
 		tableHeight = tokens[1].toInt()
 
+		val parent = this;
 		for(tileId in 0 until tableWidth*tableHeight) {
 			val readOffset = tileId+2; // Two because we have to read those integers above.
 
@@ -52,59 +61,59 @@ class Level(mapDescription: String) : Table() {
 			add(cb)
 			buttons.add(cb)
 
+			cb.addListener(object : ChangeListener() {
+				override fun changed(event: ChangeEvent?, actor: Actor?) {
+					parent.reportClick(actor as LightTile)
+				}
+			})
+
 			if((tileId+1)%tableWidth == 0) {
 				row()
 			}
 		}
 
 		this.layout()
+	}
 
-		// Attach the input handlers for this object.
-		this.addListener(object : ClickListener() {
-			override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
-				val touchStatus = super.touchDown(event, x, y, pointer, button)
-				return touchStatus
-			}
+	private fun indexToGridPosition(index:Int): Pair<Int, Int> = Pair(index%tableWidth, index/tableWidth)
+	private fun gridPositionToIndex(x:Int, y:Int): Int = x + y*tableWidth
 
-			override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
-				super.touchUp(event, x, y, pointer, button)
-				val parent:Table = event!!.listenerActor as Table
-				// Find the grid X and grid Y.
-				// Since in theory the tiles could be different sizes, iterate over the width to get the exact row.
-				val screenCoords = Vector2(Gdx.input.getX().toFloat(), Gdx.input.getY().toFloat())
-				val coords = parent.screenToLocalCoordinates(screenCoords.cpy())
-				var xPos = coords.x // Starting at the right.
-				var yPos = coords.y // And the bottom (?)
-				var gridX = -1
-				var gridY = -1
-				for(gridYTemp in 0 until parent.rows) {
-					val rowHeight = parent.getRowHeight(gridYTemp)
-					if(rowHeight < yPos) {
-						gridY = gridYTemp
-						break
-					} else {
-						yPos -= rowHeight
-					}
-				}
-				for(gridXTemp in 0 until parent.columns) {
-					val colWidth = parent.getColumnWidth(gridXTemp)
-					if(colWidth < xPos) {
-						gridX = gridXTemp
-						break
-					} else {
-						xPos -= colWidth
-					}
-				}
-				val button:LightTile? = buttons.find { it -> it.hit(x, y, false) != null }
-				println("$xPos $yPos $button $gridX $gridY")
+	private fun reportClick(from:LightTile) {
+		// If pending toggles is NOT empty, this click happened as a result of the program switching stuff.
+		if(pendingToggles.isEmpty()) {
+			// Propagate this click to the neighbors.
+			val (x, y) = indexToGridPosition(buttons.indexOf(from))
+			println("Got a report from... $from at $x $y")
+			// Interlace the items growing outwards in the cardinal directions from this point.
+			// We don't just toggle all these because we want to add JUICE!
+			for(x2 in x+1 until tableWidth) {
+				pendingToggles.add(buttons[gridPositionToIndex(x2, y)])
 			}
-		})
+			for(x2 in x-1 downTo 0) {
+				pendingToggles.add(buttons[gridPositionToIndex(x2, y)])
+			}
+			for(y2 in y+1 until tableHeight) {
+				pendingToggles.add(buttons[gridPositionToIndex(x, y2)])
+			}
+			for(y2 in y-1 downTo 0) {
+				pendingToggles.add(buttons[gridPositionToIndex(x, y2)])
+			}
+		}
 	}
 
 	fun isComplete():Boolean = buttons.all { b -> b.isChecked }
 
 	override fun act(delta: Float) {
 		super.act(delta)
+
+		if(pendingToggles.isNotEmpty() && delayToNextToggle <= 0f) {
+			pendingToggles[0].isChecked = !pendingToggles[0].isChecked // DO THIS FIRST!
+			// Pop only AFTER this is completed so we don't hit an endless loop on the last operation.
+			val lightTile = pendingToggles.removeAt(0)
+			// TODO: If lighttile is some special type
+			delayToNextToggle = delayToggle
+		}
+		delayToNextToggle -= delta
 	}
 
 	// This gets called perpetually to check if stuff has a mousedown happening.
